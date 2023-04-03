c16a0fffef7200e070f347ae7a0d8e86c364572c
mei-tsubakihara <mei19930519@gmail.com>
2023-04-03 20:59:15 +0900

    Lesson35 4/3

--- src/main/java/com/techacademy/controller/EmployeeController.java
+++ src/main/java/com/techacademy/controller/EmployeeController.java
@@ -3,7 +3,10 @@ package com.techacademy.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.techacademy.entity.Employee;
 import com.techacademy.service.EmployeeService;
 
 @Controller
@@ -15,10 +18,23 @@ public class EmployeeController {
         this.service = service;
     }
 
+    // ----- 一覧画面 -----
     @GetMapping("/")
     public String getList(Model model) {
         model.addAttribute("employeeList", service.getEmployeeList());
-        //EmployeeList.htmlに画面遷移
+        //EmployeeList.htmlに画面遷移t
         return "employee/list";
     }
+
+    // ----- 詳細画面 -----
+    @GetMapping(value = { "/detail", "/detail/{id}/" })
+    public String getEmployee(@PathVariable(name = "id", required = false) Integer id, Model model) {
+        // codeが指定されていたら検索結果、無ければ空のオブジェクトを設定
+        Employee employee = id != null ? service.getEmployee(id) : new Employee();
+        // Modelに登録
+        model.addAttribute("employee", employee);
+        // employee/detail.htmlに画面遷移
+        return "employee/detail";
+    }
+
 }
--- src/main/java/com/techacademy/service/EmployeeService.java
+++ src/main/java/com/techacademy/service/EmployeeService.java
@@ -1,6 +1,8 @@
 package com.techacademy.service;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import com.techacademy.entity.Employee;
 import com.techacademy.repository.EmployeeRepository;
@@ -15,6 +17,15 @@ public class EmployeeService {
 
     public List<Employee> getEmployeeList(){
         return employeeRepository.findAll();
+    }
+
+  // 1件を検索して返す
+    public Employee getEmployee(Integer id) {
+        // findByIdで検索
+        Optional<Employee> option = employeeRepository.findById(id);
+        // 取得できなかった場合はnullを返す
+        Employee employee = option.orElse(null);
+        return employee;
     }
 
 }

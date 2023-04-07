2269cd51554b4174d52f8ddbb9b7b21cb1b086d7
mei-tsubakihara <mei19930519@gmail.com>
2023-04-07 20:41:09 +0900

    Lesson35 4/7

--- src/main/java/com/techacademy/controller/EmployeeController.java
+++ src/main/java/com/techacademy/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.techacademy.controller;
 
 import java.time.LocalDateTime;
+import java.util.Set;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.techacademy.entity.Employee;
 import com.techacademy.service.EmployeeService;
@@ -64,9 +66,36 @@ public class EmployeeController {
 
      // ----- 更新画面 -----
     @GetMapping("/update/{id}")
-    public String getEmployee(@PathVariable("id") Integer id, Model model) {
-        
+    public String getEmployeeUpdate(@PathVariable("id") Integer id, Model model) {
+        // codeが指定されていたら検索結果、無ければ空のオブジェクトを設定
+        Employee employee = id != null ? service.getEmployee(id) : new Employee();
+        // Modelに登録
+        model.addAttribute("employee", employee);
+        // employee/update.htmlに画面遷移
+        return "employee/update";
     }
 
+    // ----- 更新処理 -----
+    @PostMapping("/update/{id}/")
+    public String postEmployeeUpdate(Employee employee) {
+        LocalDateTime now = LocalDateTime.now();
+        employee.setCreatedAt(now);
+        employee.setUpdatedAt(now);
+        employee.setDelete_flag(0);
+        employee.getAuthentication().setEmployee(employee);
+        //登録
+        service.saveEmployee(employee);
+        //一覧画面にリダイレクト
+        return "redirect:/employee/list";
+    }
+
+    // ----- 削除処理 -----
+    @PostMapping(path="list", params="deleteRun")
+    public String deleteRUn(@RequestParam(name="idck") Set<Integer> idck, Model model) {
+        //一括削除
+        service.deleteEmployee(idck);
+        //一覧画面にリダイレクト
+        return "redirect:/employee/list";
+    }
 
 }
--- src/main/java/com/techacademy/service/EmployeeService.java
+++ src/main/java/com/techacademy/service/EmployeeService.java
@@ -2,6 +2,7 @@ package com.techacademy.service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,7 +22,7 @@ public class EmployeeService {
         return employeeRepository.findAll();
     }
 
-    // 1件を検索して返す
+    // 1件を検索して返す（登録）
     public Employee getEmployee(Integer id) {
         // findByIdで検索
         Optional<Employee> option = employeeRepository.findById(id);
@@ -35,5 +36,23 @@ public class EmployeeService {
     public Employee saveEmployee(Employee employee) {
         return employeeRepository.save(employee);
     }
+
+    // 1件を検索して返す（更新）
+    public Employee getEmployeeUpdate(Integer id) {
+        // findByIdで検索
+        Optional<Employee> option = employeeRepository.findById(id);
+        // 取得できなかった場合はnullを返す
+        Employee employee = option.orElse(null);
+        return employee;
+    }
+
+    // 削除
+    @Transactional
+    public void deleteEmployee(Set<Integer> idck) {
+        for(Integer id : idck) {
+            employeeRepository.deleteById(id);
+        }
+    }
+
 
 }
